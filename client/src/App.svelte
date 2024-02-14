d04718e8165b06c811aa95de909b9e6152e9fc1f
Kasper Scott Jensen <user@IDEAPAD.KSJ>
2024-02-14 15:20:06 +0100

    svelte default setup

---
+++ client/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { Router, Route } from 'svelte-navigator'
+
+    import Index from './pages/Index.svelte'
+</script>
+
+<Router primary={false}>
+    <Route path={'/'} component={Index} />
+</Router>
